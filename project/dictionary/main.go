30e832dfb51e7445a65fe29031008c1625c5c02c
root <duaehdtjs20@gmail.com>
2021-05-29 23:34:37 +0900

    dictionary

--- project/dictionary/main.go
+++ project/dictionary/main.go
@@ -7,6 +7,16 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{}
-	dictionary["hello"] = "hello"
-	fmt.Println(dictionary)
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
+	if err != nil {
+		fmt.Println(err)
+	}
+	hello, err := dictionary.Search(word)
+	fmt.Println(hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
 }
