cc4b955b7443b4f9a14fcbfe965b19153e767da2
root <duaehdtjs20@gmail.com>
2021-05-31 23:32:20 +0900

    goroutine

--- project/goroutines/main.go
+++ project/goroutines/main.go
@@ -7,12 +7,15 @@ import (
 
 func main() {
 	c := make(chan string)
-	people := [2]string{"nico", "flynn"}
+	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	for i := 0; i < len(people); i++ {
+		fmt.Print("waiting for", i)
+		fmt.Println(<-c)
+	}
+
 }
 
 func isSexy(person string, c chan string) {
