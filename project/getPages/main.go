35f93c9c0f0e862c1ff737b2cb5ed105c2a009fe
root <duaehdtjs20@gmail.com>
2021-06-06 15:08:21 +0900

    echoFinish

--- project/getPages/main.go
+++ project/getPages/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "learnGo/project/getPages/scrapper"
+import (
+	"learnGo/project/getPages/scrapper"
+	"os"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+func handleScrape(c echo.Context) error {
+	defer os.Remove("jobs.csv")
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
+}
 
 func main() {
 
-	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":8080"))
 }
